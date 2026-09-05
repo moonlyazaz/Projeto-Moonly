4d3d68571dc338fdf4bc85a301bddaf00fd5eff6
Moonly <palomareliseuaz163@gmail.com>
2026-09-04 22:00:12 -0300

    Corrige icones quebrados no menu

---
+++ YouTube Funcional v2/fixIcons.py
@@ -0,0 +1,20 @@
+﻿import re
+
+with open('frontend/index.html', 'r', encoding='utf-8') as f:
+    html = f.read()
+
+# Update FA version
+html = html.replace('6.4.0/css/all.min.css', '6.5.1/css/all.min.css')
+html = html.replace('7.3.0/css/all.min.css', '6.5.1/css/all.min.css')
+
+# Fix broken icons
+html = html.replace('fa-solid fa-users\"', 'fa-regular fa-id-badge\"')
+html = html.replace('fa-solid fa-sign-out-alt', 'fa-solid fa-arrow-right-from-bracket')
+html = html.replace('fa-solid fa-shield-alt', 'fa-solid fa-shield-halved')
+html = html.replace('fa-solid fa-language', 'fa-solid fa-language') # This one is standard, but maybe fa-font
+html = html.replace('fa-solid fa-user-lock', 'fa-solid fa-user-shield')
+html = html.replace('fa-solid fa-globe', 'fa-solid fa-earth-americas')
+html = html.replace('fa-solid fa-cog', 'fa-solid fa-gear')
+
+with open('frontend/index.html', 'w', encoding='utf-8') as f:
+    f.write(html)
--- YouTube Funcional v2/fixN.py
+++
@@ -1,9 +0,0 @@
-﻿import re
-with open('frontend/script.js', 'r', encoding='utf-8') as f:
-    js = f.read()
-
-# Fix literal \n
-js = js.replace('\\n', '\n')
-
-with open('frontend/script.js', 'w', encoding='utf-8') as f:
-    f.write(js)
