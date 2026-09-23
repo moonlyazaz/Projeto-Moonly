f8b6a3bdf6e9d8084900182e842c2b0ccd0adde9
Equipamentos Laboratorio <compuni@edu.unifil.br>
2026-09-23 17:40:39 -0300

    commit do commit

--- AULA 06 - JS/script.js
+++ AULA 06 - JS/script.js
@@ -14,3 +14,55 @@ if (idade < 12) {
 }
 
 console.log(classificacao)
+
+
+
+
+// outra parte do codigo
+
+let dia = "sun"
+let diaTraduzido = '';
+
+switch (dia) {
+    case "mon": 
+    diaTraduzido = "Segunda-Feira"
+    break;
+    case "tue": 
+    diaTraduzido = "Terça-Feira"
+    break;
+    case "wed": 
+    diaTraduzido = "Quarta-Feira"
+    break;
+    case "thu": 
+    diaTraduzido = "Quinta-Feira"
+    break;
+    case "fri": 
+    diaTraduzido = "Sexta-Feira"
+    break;
+    case "sat": 
+    diaTraduzido = "Sábado"
+    break;
+    case "sun": 
+    diaTraduzido = "Domingo"
+    break
+    default: 
+    diaTraduzido = "Dia inexistente"
+}
+
+console.log(diaTraduzido)
+
+// Outro codigo
+
+ let nota = 90
+
+ if (nota >= 60) {
+    console.log("Aprovado!")
+ } else if (nota >= 59){
+    console.log("Recuperação!")
+ } else if (nota <= 58){
+    console.log("Reprovado Burro, Estude Mais!")
+ }
+
+ // Proximo Codigo LEGALEs
+
+ 
